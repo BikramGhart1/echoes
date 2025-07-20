e25f6950e3003dbafb7753cdecb3d984de26fb5d
BikramGhart1 <coludingrain@gmail.com>
2025-07-20 23:46:54 +0545

    add profile picture and user posts list page

---
+++ frontend/src/components/partials/UserPostsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function UserPostsList() {
+  return (
+    <div className='bg-gray-200 w-full py-4 px-4 rounded-lg flex flex-row items-start gap-x-8'>
+        <div className='w-10 h-10 rounded-full bg-gray-300'></div>
+        <div className='flex flex-col items-start flex-1 '>
+            <p className='font-bold'>@BaiNingBing11</p>
+            <p className='font-semibold'>I absolutely hate mosquitoes.</p>
+            <p>We should drive them to extinctionnnnnnnnnnnnn</p>
+        </div>
+        <p className='font-bold'>...</p>
+    </div>
+  )
+}
--- frontend/src/pages/Profilepage.jsx
+++ frontend/src/pages/Profilepage.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Header from '../components/partials/Header'
+import Homepage from '../pages/Homepage'
+import PostCard from '../components/partials/PostCard'
+import UserPostsList from '../components/partials/UserPostsList'
+
 
 export default function Profilepage() {
   return (
-    <div className='mainContainer'>
+    <div className='mainContainer pb-6'>
       <Header/>
-      <div className='flex flex-col items-center gap-y-4'>
-        <div className='h-56 w-56 overflow-hidden relative before:bg-red-400 hover:bg-red-300 transition-all duration-300 flex flex-col items-center rounded-full'>
+      <div className='relative flex flex-col items-center gap-y-4 pt-14 md:pt-16 md:mt-5 mt-1 pb-8 w-11/12 bg-gray-300 rounded-xl'>
+        <div className='md:w-6 md:h-6 w-4 h-4 bg-gray-500 rounded-xl right-1/4 md:right-1/3 absolute hover:bg-red-400 transition-all duration-300 ease-in-out'></div>
+        <div className='md:w-20 md:h-20 w-16 h-16 bg-gray-500 rounded-full right-20 md:right-1/4 md:-top-8 -top-7 absolute text-center text-white font-bold tracking-wider flex flex-row items-center z-10 cursor-pointer hover:bg-red-400 transition-all duration-300 ease-in-out'>
+        <p className='m-auto'>
+        Edit
+        </p>
+        </div>
+        <div className='relative h-48 w-48 md:h-56 md:w-56 overflow-hidden before:bg-red-400 hover:bg-red-300 transition-all duration-300 flex flex-col items-center rounded-full'>
           <img src="/catpfp.png" alt="" className='w-full h-full opacity-60 absolute inset-0 blur-sm z-0'/>
           {/* <img src="/catspider.jpg" alt="" className='w-full h-full opacity-60 absolute inset-0 blur-sm z-0'/> */}
           {/* <img src="/download.jpg" alt="" className='w-full h-full opacity-60 absolute inset-0 blur-sm z-0'/> */}
-          <div className='w-52 h-52 hover:scale-125 transition-all duration-300 ease-in-out rounded-full overflow-hidden z-10'>
+          <div className='w-44 h-44 md:w-52 md:h-52 hover:scale-125 transition-all duration-300 ease-in-out rounded-full overflow-hidden z-10'>
               {/* <img src="/download.jpg" alt="" className=''/> */}
               {/* <img src="/catspider.jpg" alt="" className='object-contain'/> */}
               <img src="/catpfp.png" alt="" className='w-full'/>
@@ -27,10 +37,21 @@ export default function Profilepage() {
         </div>
 
       </div>
-      <div className='flex flex-row justify-around rounded-2xl w-7/12 hover:bg-red-200 bg-red-100 items-center py-3 px-6'>
-        <button className='border-b-4 border-solid border-orange-400 font-semibold'>Posts</button>
-        <button className=''>Saved</button>
-        <button className=''>Comments</button>
+      <div className='flex flex-col items-center gap-y-10 rounded-xl w-11/12 bg-gray-300 pt-4 pb-8'>
+          <div className='flex flex-row justify-around sticky top-16 md:top-20 rounded-2xl w-11/12 md:w-7/12 hover:bg-red-200 bg-red-100 items-center py-2 px-6'>
+            <button className='border-b-4 border-solid border-orange-400 font-semibold'>Posts</button>
+            <button className=''>Saved</button>
+            <button className=''>Comments</button>
+          </div>
+          <div className='w-11/12 flex flex-col gap-y-4 rounded-lg bg-transparent'>
+            <UserPostsList/>
+            <UserPostsList/>
+            <UserPostsList/>
+            <UserPostsList/>
+            <UserPostsList/>
+            <UserPostsList/>
+            <UserPostsList/>
+          </div>
       </div>
     </div>
   )
